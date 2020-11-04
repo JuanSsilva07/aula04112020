3ca1a3d3304709bfca601ebcc0d438afcbd252fe
JuanSsilva07 <juan07silva@gmail.com>
2020-11-04 19:13:23 -0300

    Commit 4

--- JS/ex2.js
+++ JS/ex2.js
@@ -1,13 +1,16 @@
     /* todas as vezes que a informação vier de um input ela vem como se fosse uma string '1' = entre aspas  */
 
-document.querySelector('#btn').addEventListener('click',()=>{
+    document.querySelector('#btn').addEventListener('click',()=>{
     let n1,n2, total,totalsub,totalmulti,totaldiv;
     n1=parseInt(document.getElementById('n1').value);
     n2=parseInt(document.getElementById('n2').value);
 
     total = n1+n2;
     totalsub = n1-n2
-    alert('Resultado é '+total );
+    totalmulti = n1*n2
+    totaldiv = n1/n2
+    
+    alert('Resultado da adição: '+total +'\nresultado da subtração: '+totalsub+'\nResultado da multiplicação: '+totalmulti+'\nResultado da divisão: '+totaldiv);
 
     /* Acrescentar os cálculos de subtração (n1 - n2), multiplicação(n1*n2) e divisão (n1/n2).
     Mas tome cuidado para não deixar o usuário dividir por zero, pois é impossível.
